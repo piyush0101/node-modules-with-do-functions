ea4f9fad67799851f9e9bf692721ee4d4ebd5710
Piyush Srivastava <psrivastava@digitalocean.com>
2023-01-19 10:16:45 -0800

    initial commit

--- packages/qr/qr/qr.js
+++ packages/qr/qr/qr.js
@@ -1,9 +1,10 @@
 const qrcode = require('qrcode')
+const dt = require('./datetime.js')
 
 exports.main = (args) => {
   return qrcode.toDataURL(args.text).then(res => ({
     headers:  { 'content-type': 'text/html; charset=UTF-8' },
-    body: args.img == undefined ? res : `<img src="${res}">`
+    body: "The date and time currently are:" + dt.myDateTime()
   }))
 }
 
